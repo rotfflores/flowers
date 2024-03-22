54f85a793620da1bf78a9fe2c6dad46f9153bd5e
rotfflores <cli0001091@alumnos.ujed.mx>
2024-03-21 19:36:20 -0600

    listo

---
+++ flowers.js
@@ -0,0 +1,9 @@
+function playAudio() {
+    document.getElementById('audio').play();
+}
+
+var typed = new Typed('#element', {
+    strings: ['Él la estaba esperando con una flor amarilla', 'ella lo estaba soñando con la luz en su pupila', 'Y el amarillo del sol iluminaba la esquina', 'lo sentia tan cercano, lo sentia desde nina', 'ella sabia que el sabia, que algun dia pasaria', 'que vendria a buscarla, con sus flores amarillas'],
+    typeSpeed: 50,
+    showCursor: false,
+  });
